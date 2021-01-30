22771ab08609bec158cd727dd6fe115e79fb466f
Reymai <ricosigla@gmail.com>
2021-01-30 19:43:27 +0200

    Add Firebase to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:otras_rokas/navigation.dart';
+
+// Import the firebase_core plugin
+import 'package:firebase_core/firebase_core.dart';
+import 'package:otras_rokas/app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  // Create the initialization Future outside of `build`:
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Otras Rokas',
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-      ),
-      home: Navigation(),
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          Exception('Something went wrong');
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return App();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return CircularProgressIndicator();
+      },
     );
   }
 }
