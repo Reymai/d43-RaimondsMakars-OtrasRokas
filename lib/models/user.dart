9d3fc9cf8d4c167dbe7775eade6f1a7cd85da1bd
Reymai <ricosigla@gmail.com>
2021-02-09 23:51:25 +0200

    Update user model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 
 class User {
+  final String email;
   final String name;
   final String surname;
   final String phoneNumber;
   final NetworkImage avatar;
   final List<String> ads;
 
-  User({this.name, this.surname, this.phoneNumber, this.avatar, this.ads});
+  User({
+    this.email,
+    this.name,
+    this.surname,
+    this.phoneNumber,
+    this.avatar,
+    this.ads,
+  });
 }
