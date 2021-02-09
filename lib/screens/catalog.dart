d0bf1937b3aeab25daae45e511502757c711f810
Reymai <ricosigla@gmail.com>
2021-02-09 22:43:53 +0200

    Update catalog

--- lib/screens/catalog.dart
+++ lib/screens/catalog.dart
@@ -1,26 +1,40 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
+import 'package:otras_rokas/services/database.dart';
+
+enum CatalogItem {
+  animals,
+  clothes,
+  electrical,
+  for_home,
+  for_kids,
+  free,
+  other,
+  real_estate,
+  transport,
+  work,
+}
 
 class Catalog extends StatelessWidget {
+  final Database database = Database();
+  final List<String> categories = [];
+
   @override
   Widget build(BuildContext context) {
     List<Widget> grid = List.generate(
-      10,
-      (index) => Padding(
-        padding: EdgeInsets.all(8.0),
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: Center(
-            child: Image.network(
-                'https://via.placeholder.com/184/FFFFFF?text=${index + 1}'),
-          ),
-        ),
-      ),
-    );
+        CatalogItem.values.length,
+        (index) => Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
+                  child: Image.network(
+                      'https://via.placeholder.com/184/FFFFFF?text=${CatalogItem.values[index]}'),
+                ),
+              ),
+            ));
 
     return Scaffold(
       backgroundColor: Colors.grey[300],
